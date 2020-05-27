7d0e0879e4cc08bcd5d66c1b97324afa84cca538
Răzvan C. Rădulescu <razcore.art@gmail.com>
2020-05-27 07:25:52 +0300

    Finish removing legacy code & config

--- krita_batch_exporter/__init__.py
+++ krita_batch_exporter/__init__.py
@@ -1,7 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "Dependencies")))
 from .GDQuestBatchExporter import registerDocker  # noqa
 
 registerDocker()
